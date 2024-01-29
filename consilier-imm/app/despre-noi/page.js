a26a95bc6b2a8d3682e6f22195f1e7cc67f01cb9
Daniele Viola <daniele.viola012@gmail.ro>
2024-01-29 14:24:17 +0200

    added topbar and navbar to all pages

--- consilier-imm/app/despre-noi/page.js
+++ consilier-imm/app/despre-noi/page.js
@@ -1,7 +1,14 @@
+import Topbar from "../components/Topbar"
+import Navbar from "../components/Navbar"
+
 export default function DespreNoi () {
     return (
         <>
-        DespreNoi
+        <Topbar/>
+        <div className="h-screen">
+            <Navbar />
+
+        </div>
         </>
     )
 }
--- consilier-imm/app/servicii/page.js
+++ consilier-imm/app/servicii/page.js
@@ -1,7 +1,14 @@
+import Topbar from "../components/Topbar"
+import Navbar from "../components/Navbar"
+
 export default function Servicii () {
     return (
         <>
-        Servicii
+        <Topbar/>
+        <div className="h-screen">
+            <Navbar />
+
+        </div>
         </>
     )
 }
